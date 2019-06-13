7bed59bc4d29d1e39a4c073c1083cbc4db34504e
maxime blanc <maximeblanc.dev@gmail.com>
2019-06-13 11:53:15 +0200

    allow set no tags

--- lib/challenge/games/game.ex
+++ lib/challenge/games/game.ex
@@ -6,7 +6,7 @@ defmodule Challenge.Games.Game do
     field :description, :string
     field :image, :string
     field :name, :string
-    field :tags, {:array, :string}
+    field :tags, {:array, :string}, default: []
 
     timestamps()
   end
@@ -15,6 +15,6 @@ defmodule Challenge.Games.Game do
   def changeset(game, attrs) do
     game
     |> cast(attrs, [:name, :description, :image, :tags])
-    |> validate_required([:name, :description, :image, :tags])
+    |> validate_required([:name, :description, :image])
   end
 end
